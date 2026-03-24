517631f8bb89b84966e449604d94317d6d5c1c81
Aryan <aryanmusicbht@gmail.com>
2026-03-25 00:21:56 +0530

    first commit

---
+++ num.py
@@ -0,0 +1,37 @@
+import numpy as np 
+
+# array = np.array([[[1,2,3],[1,2,3],[1,2,3]],
+#                  [[1,2,3],[1,2,3],[1,2,3]],
+#                  [[1,2,3],[1,2,3],[1,2,3]]])
+
+# print(array.ndim)
+# print(array.shape)
+# print(array[2,2,1])
+
+#1.Slicing
+array = np.array([[1,2,3,4,],
+                  [5,6,7,8,],
+                  [9,10,11,12],
+                  [13,14,15,16]])
+# print(array.ndim)
+# print(array.shape)
+#Row Selection 
+#array[start:end:step]
+
+# print(array[0:4:2])
+# print(array[0:3:2])
+# print(array[::-1])
+
+#Column Selection
+
+# print(array[:,0])
+# print(array[:,0:4])
+# print(array[:,1:3])
+# print(array[:,::2])
+print(array[0:2,0:2])
+
+
+
+
+
+
